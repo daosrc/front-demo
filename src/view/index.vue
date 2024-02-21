924f7ae79af7b00f29cb5f57f4a28d82611e0dc9
daosrc <daosrc@outlook.com>
2024-02-21 17:15:30 +0800

    add file

---
+++ src/view/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Your Vue 3 template code will go here -->
+</template>
+
+<script setup>
+// Your Composition API script code will go here
+</script>
+
+<style scoped>
+/* Your scoped CSS styles will go here */
+</style>
